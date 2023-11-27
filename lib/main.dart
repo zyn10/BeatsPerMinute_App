8f3ef5fd0def2939d92f0e602220777a6b69d676
Muhammad Zain <zainandcs@gmail.com>
2023-11-27 20:26:21 +0500

    audio player pkg

--- lib/main.dart
+++ lib/main.dart
@@ -13,7 +13,9 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        colorScheme: ColorScheme.fromSeed(
+          seedColor: Colors.deepPurple,
+        ),
         useMaterial3: true,
       ),
       home: const BPMCalculator(),
